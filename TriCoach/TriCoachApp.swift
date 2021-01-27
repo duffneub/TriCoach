612a05045792b54bd0bc16c7aad01b089ac1747b
Duff Neubauer <d.neubauer@me.com>
2021-01-26 18:11:45 -0800

    Add Xcode project

---
+++ TriCoach/TriCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriCoachApp.swift
+//  TriCoach
+//
+//  Created by Duff Neubauer on 1/26/21.
+//
+
+import SwiftUI
+
+@main
+struct TriCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
